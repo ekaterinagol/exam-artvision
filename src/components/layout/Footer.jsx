8ba4fc7eb5cdeb6983c9fab59a84953ed6d83e81
ekaterinagol <103747746+ekaterinagol@users.noreply.github.com>
2026-08-12 00:07:23 +0500

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './footer.module.css';
-import { Link } from "react-router-dom"; 
+import { Link, useLocation } from "react-router-dom"; 
 
 function Footer() {
     return (
